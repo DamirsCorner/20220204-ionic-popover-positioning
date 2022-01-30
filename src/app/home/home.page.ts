3378883104751d253b75b432b2eca528687b33ca
Damir Arh <damir.arh@gmail.com>
2022-01-30 09:52:37 +0100

    Auto-positioned popover

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public fakeItems = Array.from(Array(10).keys());
+  private dateValue: any;
 
   constructor() {}
 
+  get date(): any {
+    return this.dateValue;
+  }
+  set date(value: any) {
+    console.log({ value });
+    this.dateValue = value;
+  }
 }
